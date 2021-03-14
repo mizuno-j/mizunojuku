ce2da28e59a168b53f8cea2c96a1b338487d7d72
mizuno-j <77427738+mizuno-j@users.noreply.github.com>
2021-03-14 10:48:31 +0900

    Add files via upload

--- js/slideshow.js
+++ js/slideshow.js
@@ -42,3 +42,16 @@ const mySwiper = new Swiper('.swiper-container', {
   //   el: '.swiper-scrollbar',
   // },
 })
+
+
+var body = document.getElementById('body');
+var openMenu = document.getElementById('menuControlOpen'); // or whatever triggers the toggle
+var closeMenu = document.getElementById('menuControlClose'); // or whatever triggers the toggle
+
+openMenu.addEventListener('click', function(e) {
+   body.classList.toggle('menu--active'); // or whatever your active class is
+});
+
+ closeMenu.addEventListener('click', function(e) {
+    body.classList.toggle('menu--active'); // or whatever your active class is
+});
